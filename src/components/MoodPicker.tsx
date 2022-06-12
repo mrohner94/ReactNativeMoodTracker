5b4f99c730924f6e0df21077334e7b1365d3fe3b
Mike <mrohner320@gmail.com>
2022-06-12 19:23:18 -0400

    general screens, theme, components

---
+++ src/components/MoodPicker.tsx
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
+import { MoodOptionType } from '../types';
+import { theme } from '../theme';
+
+const moodOptions: MoodOptionType[] = [
+  { emoji: '🧑‍💻', description: 'studious' },
+  { emoji: '🤔', description: 'pensive' },
+  { emoji: '😊', description: 'happy' },
+  { emoji: '🥳', description: 'celebratory' },
+  { emoji: '😤', description: 'frustrated' },
+];
+
+export const MoodPicker: React.FC = () => {
+  const [selectedMood, setSelectedMood] = useState<MoodOptionType>();
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>How are you right now?</Text>
+      <View style={styles.moodOptions}>
+        {moodOptions.map(option => (
+          <View>
+            <Pressable
+              onPress={() => setSelectedMood(option)}
+              key={option.emoji}
+              style={[
+                styles.moodItem,
+                option.emoji === selectedMood?.emoji
+                  ? styles.selectedMoodItem
+                  : undefined,
+              ]}>
+              <Text style={styles.moodText}>{option.emoji}</Text>
+            </Pressable>
+            <Text style={styles.descriptionText}>
+              {selectedMood?.emoji === option.emoji ? option.description : ' '}
+            </Text>
+          </View>
+        ))}
+      </View>
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Choose</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  moodOptions: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  moodItem: {
+    height: 60,
+    width: 60,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  selectedMoodItem: {
+    backgroundColor: theme.colorPurple,
+    borderColor: theme.colorWhite,
+    borderWidth: 2,
+  },
+  descriptionText: {
+    color: theme.colorPurple,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 10,
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    letterSpacing: 1,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  container: {
+    borderWidth: 2,
+    borderColor: theme.colorPurple,
+    margin: 10,
+    borderRadius: 10,
+    padding: 20,
+  },
+  button: {
+    backgroundColor: theme.colorPurple,
+    width: 150,
+    borderRadius: 20,
+    marginTop: 20,
+    alignSelf: 'center',
+    padding: 10,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  moodText: {},
+});
---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+export const theme = {
+  colorPurple: '#454C73',
+  colorWhite: '#FFF',
+
+  //space10: 10,
+  //space12: 12,
+};
