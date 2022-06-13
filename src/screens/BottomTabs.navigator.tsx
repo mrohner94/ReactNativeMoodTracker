da6672ed923e761cc33c95335a2a00ffa1586b2f
Mike <mrohner320@gmail.com>
2022-06-12 23:11:44 -0400

    updated themes, navigator, icons

--- src/screens/BottomTabs.navigator.tsx
+++ src/screens/BottomTabs.navigator.tsx
@@ -3,15 +3,47 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { History } from './History.screen';
 import { Home } from './Home.screen';
 import { Analytics } from './Analytics.screen';
+import { Text } from 'react-native';
+import { HomeIcon, ListIcon, AnalyticsIcon } from '../components/Icons';
+import { theme } from '../theme';
 
 const BottomTabs = createBottomTabNavigator();
 
 export const BottomTabsNavigator: React.FC = () => {
   return (
-    <BottomTabs.Navigator>
-      <BottomTabs.Screen name="Home" component={Home} />
-      <BottomTabs.Screen name="History" component={History} />
-      <BottomTabs.Screen name="Analytics" component={Analytics} />
+    <BottomTabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: theme.colorBlue,
+        tabBarInactiveTintColor: theme.colorGrey,
+        tabBarShowLabel: false,
+        tabBarIcon: ({ color, size }) => {
+          switch (route.name) {
+            case 'Home':
+              return <HomeIcon size={size} color={color} />;
+            case 'History':
+              return <ListIcon size={size} color={color} />;
+            case 'Analytics':
+              return <AnalyticsIcon size={size} color={color} />;
+            default:
+              return null;
+          }
+        },
+      })}>
+      <BottomTabs.Screen
+        name="Home"
+        component={Home}
+        options={{ title: "Today's mood" }}
+      />
+      <BottomTabs.Screen
+        name="History"
+        component={History}
+        options={{ title: 'Past Moods' }}
+      />
+      <BottomTabs.Screen
+        name="Analytics"
+        component={Analytics}
+        options={{ title: 'Fancy Graphs', headerShown: false }}
+      />
     </BottomTabs.Navigator>
   );
 };
